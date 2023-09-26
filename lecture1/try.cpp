0ed4e509db773f61121ba0d1e806d4ca9c390c7a
Edoardo Insaghi <72695438+edoardoInsaghi@users.noreply.github.com>
2023-09-26 15:13:02 +0200

    third commit

---
+++ lecture1/try.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+     std::cout << "Hello, World!";
+     return 0;
+}
+
+
+/* 
+
+Introductory lesson covering:
+
++ history of c++
++ history of Python
++ the build process
++ the Unix Shell
++ git
+
+*/
+
+
+
+
